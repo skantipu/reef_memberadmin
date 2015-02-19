bbc6c65d9e6e1530ceb522660fdf523d60faaf1f
skantipu <skantipu@eng.ucsd.edu>
2015-02-19 06:54:37 -0800

    Update index.php

--- index.php
+++ index.php
@@ -20,8 +20,8 @@
 </head>
 <body style="background-color: #f7f7f7">
 	<?php
-	$temp=0; // a temporary variable. It will be set for wrong user credentials.
-	include 'config.php'; //has database connection related global constants defined in it
+	$temp=0; // a temporary variable. It will be set for wrong user credentials.temp = 1 if wrong user credentials and temp=2 for inactive user. It is used for displaying appropriate error message later in form body
+	include 'config.php'; //config.php has database connection related global constants defined in it
 	if(isset($_POST['login'])) //if button 'Login' is clicked
 	{
 		$con=mysqli_connect(DB_SERVER,DB_USER,DB_PASSWORD,DB_NAME);
@@ -29,19 +29,27 @@
 		{
 			echo "Failed to connect to MySQL: " . mysqli_connect_error();
 		}
+		//sanitize user input
+		$uname = mysqli_real_escape_string($con, $_POST["username"]);
+		$pwd = mysqli_real_escape_string($con, $_POST["password"]);
 		
-		$uname=$_POST["username"];
-		$pwd=$_POST["password"];
+		//encrypt user password using md5 algorithm
 		$pwd=md5($pwd);
+		
+		//check if there is a match in user_table with the entered user credentials. If there is a match, one row (count = 1) should be retreieved 
 		$result = mysqli_query($con,"SELECT * FROM user_table WHERE username = '$uname' and password = '$pwd'");
 		$row = mysqli_fetch_array($result);
-		$count=mysqli_num_rows($result);
+		$count = mysqli_num_rows($result);
+		
+		//if user name and password matches (count = 1), check if user is active
 		if($count==1)
 		{
+			//if user is not active, initialize temp=2, display appropriate error message and not let him proceed, done later in form body
 			if($row['active']!=1)
 			{
-				$temp=2;  //user in no longer active
+				$temp=2; 
 			}
+			//if user is active, initialize all session variables and redirect to homepage
 			else
 			{
 				
@@ -50,13 +58,13 @@
 				$_SESSION['firstname']= $row['firstname'];
 				$_SESSION['lastname']= $row['lastname'];
 				$_SESSION['id']=$row['id'];
-				echo "h1jjjj";
 				header('Location: /a1/homepage.php');	
 			}
 		} //close of outer if 
+		//user name and password do not match, set temp variable to 1
 		else
 		{
-			$temp=1;  //username and pwd do not match
+			$temp=1;  
 		}
 	mysqli_close($con);
 	}	
@@ -71,7 +79,7 @@
 				if($temp==1){  //wrong user credentials
 					echo '<div id="error">Username and Password do not match...Please try again!</div>';
 				}
-				else if($temp==2){
+				else if($temp==2){ //inactive user
 					echo '<div id="error">Sorry, you are not an active user. Please contact your supervisor for help!</div>';
 				}
 				?>
